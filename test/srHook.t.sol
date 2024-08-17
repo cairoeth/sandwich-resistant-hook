93ff20053167f570c8ff208e799b13fbb2aaeb1f
cairo <cairoeth@protonmail.com>
2024-08-17 16:15:38 -0700

    ✅ test for sandwich in opposite direction

--- test/srHook.t.sol
+++ test/srHook.t.sol
@@ -12,15 +12,11 @@ import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {Currency} from "v4-core/src/types/Currency.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
-import {SafeCast} from "v4-core/src/libraries/SafeCast.sol";
 import {ERC20} from "solmate/src/tokens/ERC20.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
-import {Pool} from "v4-core/src/libraries/Pool.sol";
 import {PoolModifyLiquidityTest} from "v4-core/src/test/PoolModifyLiquidityTest.sol";
 
 contract srHookTest is Test, Deployers {
-    using SafeCast for *;
-
     srHook hook;
     PoolKey _key;
     PoolModifyLiquidityTest _modifyLiquidityRouter;
@@ -147,7 +143,55 @@ contract srHookTest is Test, Deployers {
         });
 
         delta = swapRouter.swap(_key, params, testSettings, ZERO_BYTES);
-        assertGe(deltaEnd.amount0(), 996911360539219, "state did not reset");
+        // 996911360539219 is obtained from `test_swap_successfulSandwich`
+        assertEq(delta.amount1(), 996911360539219, "state did not reset");
+    }
+
+    /// @notice Unit test for a failed sandwich attack using the hook, in the opposite direction.
+    function test_swap_failedSandwich_opposite() public {
+        // add liquidity to hook and poolmanager
+        _modifyLiquidityRouter.modifyLiquidity(
+            _key,
+            IPoolManager.ModifyLiquidityParams({tickLower: -120, tickUpper: 120, liquidityDelta: 1e18, salt: 0}),
+            ZERO_BYTES
+        );
+        initPool(currency0, currency1, IHooks(address(hook)), 100, SQRT_PRICE_1_1, ZERO_BYTES);
+
+        uint256 amountToSwap = 1e15;
+        PoolSwapTest.TestSettings memory testSettings =
+            PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false});
+        IPoolManager.SwapParams memory params = IPoolManager.SwapParams({
+            zeroForOne: false,
+            amountSpecified: -int256(amountToSwap),
+            sqrtPriceLimitX96: MAX_PRICE_LIMIT
+        });
+        // buy currency0 for currency1, front run
+        BalanceDelta delta = swapRouter.swap(_key, params, testSettings, ZERO_BYTES);
+
+        // sandwiched buy currency0 for currency1
+        swapRouter.swap(_key, params, testSettings, ZERO_BYTES);
+
+        // sell currency1 for currency0, front run
+        params = IPoolManager.SwapParams({
+            zeroForOne: true,
+            amountSpecified: -int256(delta.amount0()),
+            sqrtPriceLimitX96: MIN_PRICE_LIMIT
+        });
+        BalanceDelta deltaEnd = swapRouter.swap(_key, params, testSettings, ZERO_BYTES);
+
+        assertLe(deltaEnd.amount1(), -delta.amount1(), "front runner profit");
+
+        vm.roll(block.number + 1);
+
+        params = IPoolManager.SwapParams({
+            zeroForOne: false,
+            amountSpecified: -int256(amountToSwap),
+            sqrtPriceLimitX96: MAX_PRICE_LIMIT
+        });
+
+        delta = swapRouter.swap(_key, params, testSettings, ZERO_BYTES);
+        // 996911360539219 is obtained from `test_swap_successfulSandwich`
+        assertEq(delta.amount0(), 996911360539219, "state did not reset");
     }
 
     /// @notice Unit test for a successful sandwich attack without using the hook.
@@ -180,6 +224,41 @@ contract srHookTest is Test, Deployers {
             zeroForOne: true,
             amountSpecified: -int256(amountToSwap),
             sqrtPriceLimitX96: MIN_PRICE_LIMIT
+        });
+
+        swapRouter.swap(key, params, testSettings, ZERO_BYTES);
+    }
+
+    /// @notice Unit test for a successful sandwich attack without using the hook, in the opposite direction.
+    function test_swap_successfulSandwich_opposite() public {
+        uint256 amountToSwap = 1e15;
+        PoolSwapTest.TestSettings memory testSettings =
+            PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false});
+        IPoolManager.SwapParams memory params = IPoolManager.SwapParams({
+            zeroForOne: false,
+            amountSpecified: -int256(amountToSwap),
+            sqrtPriceLimitX96: MAX_PRICE_LIMIT
+        });
+        // buy currency0 for currency1, front run
+        BalanceDelta delta = swapRouter.swap(key, params, testSettings, ZERO_BYTES);
+
+        // sandwiched buy currency0 for currency1
+        swapRouter.swap(key, params, testSettings, ZERO_BYTES);
+
+        // sell currency1 for currency0, front run
+        params = IPoolManager.SwapParams({
+            zeroForOne: true,
+            amountSpecified: -int256(delta.amount0()),
+            sqrtPriceLimitX96: MIN_PRICE_LIMIT
+        });
+        BalanceDelta deltaEnd = swapRouter.swap(key, params, testSettings, ZERO_BYTES);
+
+        assertGe(deltaEnd.amount1(), -delta.amount1(), "front runner loss");
+
+        params = IPoolManager.SwapParams({
+            zeroForOne: false,
+            amountSpecified: -int256(amountToSwap),
+            sqrtPriceLimitX96: MAX_PRICE_LIMIT
         });
 
         swapRouter.swap(key, params, testSettings, ZERO_BYTES);
